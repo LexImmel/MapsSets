6d07edc46e035a07ee89b53d7f84b73907bb1ff3
LexImmel <chiffa91@ymail.com>
2024-11-09 22:05:13 +0500

    init

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
     private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
 
-
     public static void main(String[] args) {
         taskOne();
         taskTwo();
